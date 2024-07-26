767c6b655446b959a6c30982c0415eefa4091a21
Daniel Souza <workdndsza@gmail.com>
2024-07-26 09:13:33 -0300

    criando o diretorio para listagem

--- src/main/java/com/dnsouzadev/poi/controller/PointOfInterestController.java
+++ src/main/java/com/dnsouzadev/poi/controller/PointOfInterestController.java
@@ -1,12 +1,18 @@
 package com.dnsouzadev.poi.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.dnsouzadev.poi.controller.dto.CreatePointOfInterest;
 import com.dnsouzadev.poi.entity.PointOfInterest;
 import com.dnsouzadev.poi.repository.PointOfInterestRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class PointOfInterestController {
@@ -22,5 +28,12 @@ public class PointOfInterestController {
         repository.save(new PointOfInterest(body.name(), body.x(), body.y()));
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/poi")
+    public ResponseEntity<Page<PointOfInterest>> listPoi(@RequestParam(name="page", defaultValue="0") Integer page, @RequestParam(name="pageSize", defaultValue="10") Integer pageSize) {
+        var body = repository.findAll(PageRequest.of(page, pageSize));
+        return ResponseEntity.ok(body);
+    }
+
 
 }
