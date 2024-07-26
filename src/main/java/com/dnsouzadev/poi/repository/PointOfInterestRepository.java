8f8a152ab362dc97cb7323d3900af21efd398235
Daniel Souza <workdndsza@gmail.com>
2024-07-26 08:57:13 -0300

    criando o repository

---
+++ src/main/java/com/dnsouzadev/poi/repository/PointOfInterestRepository.java
@@ -0,0 +1,7 @@
+package com.dnsouzadev.poi.repository;
+
+import com.dnsouzadev.poi.entity.PointOfInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long> {
+}
